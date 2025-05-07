9c2af2f77abe491e708c325a6b358fd6b66309b7
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-05-07 09:47:36 +0200

    🔌ConnectionDB created.

--- src/main/java/com/boardgamemanager/controller/Main.java
+++ src/main/java/com/boardgamemanager/controller/Main.java
@@ -1,10 +1,15 @@
 package com.boardgamemanager.controller;
 
+import com.boardgamemanager.model.ConectionDB;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("✅ Proyecto listo y funcionando.");
-
+		ConectionDB conectionDB = new ConectionDB ("Java", "Hola12345", "localhost", "3306", "School_DB");
+		//Abrir la vista
+		//MainView view = new MainView(); // Crea e inicializa la ventana
+	    //view.setVisible(true);
 	}
 
 }
